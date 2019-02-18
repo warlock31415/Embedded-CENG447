4bee696889e216b5db8e262c1bba1d3470264547
warlock31415 <pratikkunkolienkar@gmail.com>
2019-02-17 21:32:18 -0700

    Add files via upload

--- Lab2/Lab2/main.asm
+++ Lab2/Lab2/main.asm
@@ -14,10 +14,10 @@
 .def uart_buf2 = r19
 
 ;Initialize SP
-LDI	ZH, low(RAMEND)
-OUT	SPL, ZH
-LDI	ZH, high(RAMEND)
-OUT	SPH, ZH
+LDI	r23, low(RAMEND)
+OUT	SPL, r23
+LDI	r23, high(RAMEND)
+OUT	SPH, r23
 
 UART_INIT:
 	push uart_buf
@@ -41,13 +41,15 @@ UART_INIT:
 
 main:	
 	lds r17, UCSR0A
-	sbrc r17, RXC0
-		rjmp UART_RX
-	
-	rcall BLINK
+	;sbrc r17, RXC0
+		;rjmp UART_RX
+	andi out_buf,0b00000111 ;Mask to convert ASCII 0-7 to binary
+	rcall UART_TX ;Transmit over UART
+	rcall BLINK ;Turn on LEDS 
+	rcall DELAY ;wait 1 second
+	rcall UNBLINK ;Turn off LEDs
 	rcall DELAY
-	rcall UNBLINK
-	rcall DELAY
+	inc r16
 	rcall main
 
 UART_TX:
@@ -60,41 +62,35 @@ UART_TX:
 	ret
 
 UART_RX:
-	push r17
-	lds r17,UCSR0A
-	;sbrs r17, RXC0
-		;rjmp UART_RX
 	lds out_buf, UDR0
-	pop r17
-	rjmp UART_TX
 	ret
 
 DELAY: 
-	ldi XH,0x01
-	ldi XL,0x04
-	ldi YH,0x53
+	ldi r21,0x01
+	ldi r22,0x04
+	ldi r23,0x53
 	DELAY_LOOP:
-		dec XH     ; 1 clk cycle
+		dec r21     ; 1 clk cycle
 		brne DELAY_LOOP ; 2 clk cycles (1 if true)
-		dec XL     ; 1 clc cycle
+		dec r22    ; 1 clc cycle
 		brne DELAY_LOOP ; 2 clk cycles
-		dec YH
+		dec r23
 		brne DELAY_LOOP
 	ret  
 	
 
 BLINK:
 	push r17
-	ldi r17,0xFF
-	out DDRB,r17
-	out PORTB,r16
+	ldi r17,0x07 ;Set first 3 bits of DDRB to output
+	out DDRB,r17 ;
+	out PORTB,out_buf ;Write the pressed number to LEDs
 	pop r17
 	ret
 
 UNBLINK:
 	push r17
 	clr r17
-	out PORTB,r17
+	out PORTB,r17 ;Clear all pins on PORTB
 	pop r17
 	ret
 
