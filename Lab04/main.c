4d638289790c3cbd53a215c6c113e7f28ab3a96d
warlock31415 <pratikkunkolienkar@gmail.com>
2019-03-23 13:52:53 -0600

    Completed read pins 9/11

--- Lab04/main.c
+++ Lab04/main.c
@@ -1,5 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
+
 
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE F_CPU/(USART_BAUDRATE*16UL) -1
@@ -7,9 +9,13 @@
 void init();
 void outputstring(char *);
 void receviestring(char *);
+void get_read_write(char *);
+void read(char *);
+void error();
 
 int main(void)
 {
+
 	char menu[][100] = 
 	{
 		"\nCOMMAND\t\tPIN_NUM\t\tSTATE\r\n",
@@ -17,19 +23,25 @@ int main(void)
 		"WRITE\t\t8/10\t\tHIGH/LOW\r\n"
 	};
 
+	char prompt[] = ">>";
+
 	char command[100] = "\0";
 
 	init();
-		while(1)
-	{
 		for(int i=0;i<3;i++)
 		{
 			outputstring(menu[i]);
-		}
-		//delay_ms(1000);		
+		}	
+		
+	while(1)
+	{
+		outputstring(prompt);
 		receviestring(command);
 
-		outputstring(command);
+		get_read_write(command);
+
+
+		//outputstring(command);
 	}
 	return 1;
 }
@@ -40,6 +52,8 @@ void init(){
 
 	UBRR0L = BAUD_PRESCALE;
 	UBRR0H = ((BAUD_PRESCALE)>>8);
+
+	DDRD = 0x0A; // Set pins 8 and 10 to outputs, set 9 and 10 as inputs
 }
 
 void outputstring(char *str){
@@ -57,4 +71,44 @@ void receviestring(char *str){
 		str[i++] = UDR0;
 	}
 	str[i] = '\0';
+}
+
+void get_read_write(char *str){
+
+	if(strstr(str,"READ "))
+	{
+		read(str);
+	}
+	else if(strstr(str,"WRITE "))
+	{
+
+	}
+	else
+	{
+		error();	
+	}
+}
+
+void read(char *str){
+	outputstring("In read\r\n");
+	if(strstr(str," 9"))
+		{
+			if(PINB & 1<<PINB1)
+				outputstring("Pin 9 reads HIGH");
+			else
+				outputstring("Pin 9 reads LOW");
+		}
+	else if(strstr(str," 11"))
+			if(PINB & 1<<PINB3)
+				outputstring("Pin 11 reads HIGH");
+			else
+				outputstring("Pin 11 reads LOW");
+	else
+		error();
+}
+
+
+void error()
+{
+	outputstring("INVALID STATE. Command structure: READ/(WRITE) PIN_NUM (HIGH/LOW)\r\n");
 }
