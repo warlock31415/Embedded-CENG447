b6ecb4f8cf0216fe865da5f67e50a5b8e9b50619
warlock31415 <pratikkunkolienkar@gmail.com>
2019-03-23 14:23:09 -0600

    Need to add documentation

--- Lab04/main.c
+++ Lab04/main.c
@@ -12,6 +12,7 @@ void receviestring(char *);
 void get_read_write(char *);
 void read(char *);
 void error();
+void write();
 
 int main(void)
 {
@@ -19,8 +20,10 @@ int main(void)
 	char menu[][100] = 
 	{
 		"\nCOMMAND\t\tPIN_NUM\t\tSTATE\r\n",
+		"**************************************\r\n",
 		"READ\t\t9/11\t\t-\r\n",
-		"WRITE\t\t8/10\t\tHIGH/LOW\r\n"
+		"WRITE\t\t8/10\t\tHIGH/LOW\r\n",
+		"**************************************\r\n"
 	};
 
 	char prompt[] = ">>";
@@ -28,7 +31,7 @@ int main(void)
 	char command[100] = "\0";
 
 	init();
-		for(int i=0;i<3;i++)
+		for(int i=0;i<5;i++)
 		{
 			outputstring(menu[i]);
 		}	
@@ -54,6 +57,7 @@ void init(){
 	UBRR0H = ((BAUD_PRESCALE)>>8);
 
 	DDRD = 0x0A; // Set pins 8 and 10 to outputs, set 9 and 10 as inputs
+	PORTB = 0X00;
 }
 
 void outputstring(char *str){
@@ -81,7 +85,7 @@ void get_read_write(char *str){
 	}
 	else if(strstr(str,"WRITE "))
 	{
-
+		write(str);
 	}
 	else
 	{
@@ -91,18 +95,49 @@ void get_read_write(char *str){
 
 void read(char *str){
 	outputstring("In read\r\n");
-	if(strstr(str," 9"))
-		{
+	if(strstr(str," 9")){
 			if(PINB & 1<<PINB1)
-				outputstring("Pin 9 reads HIGH");
+				outputstring("Pin 9 reads HIGH\r\n");
 			else
-				outputstring("Pin 9 reads LOW");
+				outputstring("Pin 9 reads LOW\r\n");
 		}
-	else if(strstr(str," 11"))
+	else if(strstr(str," 11")){
 			if(PINB & 1<<PINB3)
-				outputstring("Pin 11 reads HIGH");
+				outputstring("Pin 11 reads HIGH\r\n");
 			else
-				outputstring("Pin 11 reads LOW");
+				outputstring("Pin 11 reads LOW\r\n");
+		}
+	else
+		error();
+}
+
+void write(char *str){
+	outputstring("In write\r\n");
+
+	if (strstr(str," 8 ")){
+		if (strstr(str,"HIGH")){
+			outputstring("Pin 8 is set HIGH\r\n");
+			PORTB |= (1<<PINB0);
+		}
+		else if(strstr(str, "LOW")){
+			outputstring("Pin 8 is set LOW\r\n");
+			PORTB &= 0b11111110;
+		}
+		else
+			error();
+	}
+	else if(strstr(str," 10 ")){
+		if (strstr(str,"HIGH")){
+			outputstring("Pin 10 is set HIGH\r\n");
+			PORTB |= (1<<PINB2);
+		}
+		else if(strstr(str,"LOW")){
+			outputstring("Pin 10 is set LOW\r\n");
+			PORTB &= 0B11111011;
+		}
+		else
+			error();
+	}
 	else
 		error();
 }
